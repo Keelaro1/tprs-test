85a85c75a196dae3294650a994f81de919e9aaba
Anton Vorobyov <igelness@gmail.com>
2019-09-13 22:42:44 +0300

    FIX dropdown behavior + minor fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-$(".header__help-link").click(function() {
-  $(".header__help-dropdown").toggle(300);
+$(function() {
+  $(".header__help-link").click(function() {
+    $(".header__help-dropdown").toggle(300);
+  });
+
+  $(document).click(function(e) {
+    if (e.target.id != ".header__help-link" &&
+      $(e.target).parents(".header__help-link").length == 0) {
+      $(".header__help-dropdown").hide(300);
+    }
+  });
 });
