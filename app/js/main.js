90212d6a650445044143bf16cce673bc40119374
Anton Vorobyov <igelness@gmail.com>
2019-09-16 04:37:35 +0300

    ADD mobile accordion

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,33 @@ $(function() {
 
 if ($(window).width() < 769) {
   $(".header__icon-bars").toggleClass("header__icon-bars-changed");
+
   $(".header__btn-menu").click(function() {
     $(".header__icon-bars").toggleClass("header__icon-bars-changed");
     $(".header__help-dropdown").toggle(300);
   });
 }
+
+
+if ($(window).width() < 769) {
+  $(".payment__inner-title").text("Узнайте, как оплатить");
+  $(".payment__penalties-mobile").css("border-bottom", "1px dotted #0777ba")
+  $(".payment__penalties-mobile").text ("Штрафы ГИБДД");
+}
+
+$('.toggle').click(function(e) {
+  e.preventDefault();
+  var $this = $(this);
+
+  if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+  } else {
+      $this.parent().parent().find('li .inner').removeClass('show');
+      $this.parent().parent().find('li .inner').slideUp(350);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350);
+  }
+});
+
+                
