25ca40bf99b00b609f6443e9bde713203a83abd7
Anton Vorobyov <igelness@gmail.com>
2019-09-16 20:19:10 +0300

    ADD footer mobile

--- app/js/main.js
+++ app/js/main.js
@@ -5,21 +5,21 @@ $(function() {
   });
 });
 
-if ($(window).width() < 769) {
+$(function() {
   $(".header__icon-bars").toggleClass("header__icon-bars-changed");
 
   $(".header__btn-menu").click(function() {
     $(".header__icon-bars").toggleClass("header__icon-bars-changed");
     $(".header__help-dropdown").toggle(300);
   });
-}
-
+});
 
 if ($(window).width() < 769) {
   $(".payment__inner-title").text("Узнайте, как оплатить");
   $(".payment__penalties-mobile").css("border-bottom", "1px dotted #0777ba")
   $(".payment__penalties-mobile").text ("Штрафы ГИБДД");
 }
+
 
 $('.toggle').click(function(e) {
   e.preventDefault();
