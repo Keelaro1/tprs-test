b746dbf4ee40b4104b93845b2d317e0fe230102f
Anton Vorobyov <igelness@gmail.com>
2019-09-17 16:42:56 +0300

    FIX everything

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,14 @@
 $(function() {
+ 
   $(".header__help-link").click(function() {
-    $(".header__help-dropdown").toggle(300);
-    $(".header__icon-bars").toggleClass("header__icon-bars-changed");
+    $(".header__help-dropdown").toggleClass("desktop-dropdown");
   });
+
+  $('html').click(function(event) {
+    if ($(event.target).closest('.header__help-dropdown, .header__help-link').length === 0) {
+        $('.header__help-dropdown').addClass("desktop-dropdown");
+    }
+ });
 });
 
 $(function() {
@@ -10,30 +16,40 @@ $(function() {
 
   $(".header__btn-menu").click(function() {
     $(".header__icon-bars").toggleClass("header__icon-bars-changed");
-    $(".header__help-dropdown").toggle(300);
+    $(".header__help-dropdown").toggleClass("mobile-dropdown");
   });
+
+  $('html').click(function(event) {
+    if ($(event.target).closest('.header__help-dropdown, .header__icon-bars').length === 0) {
+      $(".header__icon-bars").removeClass("header__icon-bars-changed");
+      $(".header__help-dropdown").addClass("mobile-dropdown");
+    }
+ });
+
 });
 
-if ($(window).width() < 769) {
-  $(".payment__inner-title").text("Узнайте, как оплатить");
-  $(".payment__penalties-mobile").css("border-bottom", "1px dotted #0777ba")
-  $(".payment__penalties-mobile").text ("Штрафы ГИБДД");
-}
 
-
-$('.toggle').click(function(e) {
+$(".toggle").click(function(e) {
   e.preventDefault();
   var $this = $(this);
 
-  if ($this.next().hasClass('show')) {
-      $this.next().removeClass('show');
-      $this.next().slideUp(350);
+  $this.prev().toggleClass("triangle-open");
+
+  if ($this.next().hasClass("show")) {
+    $this.next().removeClass("show");
+    $this.next().slideUp(350);
   } else {
-      $this.parent().parent().find('li .inner').removeClass('show');
-      $this.parent().parent().find('li .inner').slideUp(350);
-      $this.next().toggleClass('show');
-      $this.next().slideToggle(350);
+    $this
+      .parent()
+      .parent()
+      .find("li .inner")
+      .removeClass("show");
+    $this
+      .parent()
+      .parent()
+      .find("li .inner")
+      .slideUp(350);
+    $this.next().toggleClass("show");
+    $this.next().slideToggle(350);
   }
 });
-
-                
